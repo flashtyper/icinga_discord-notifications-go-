dfdeb1b382922560a3042f2d21de2a0decd97f3f
Lukas <83031404+flashtyper@users.noreply.github.com>
2021-10-16 00:57:31 +0200

    Create discord-notification.go

---
+++ discord-notification.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+
+func main() {
+    argsWithoutProg := os.Args[1:]
+    icingaMap := make(map[string]string)
+    for i := 0; i < len(argsWithoutProg); i +=2 {
+        icingaMap[argsWithoutProg[i]] = argsWithoutProg[i+1]
+    }
+
+    if icingaMap["notification_type"] == "CUSTOM" {
+        custom(icingaMap)
+    } else {
+        host_or_service(icingaMap)
+    }
+}
+
+func custom (icingaMap map [string]string) {
+        emote := get_emote(icingaMap["notification_type"])
+        notification := fmt.Sprintf("%v %v: %v%v%v%v%v%v%v%v%v: %v%v%v%v", emote, icingaMap["notification_type"], "Service **", icingaMap["service_name"], "** on host **", icingaMap["host_name"], "** is in state **", icingaMap["service_state"], "**!\n", "```", icingaMap["notification_author"], icingaMap["notification_comment"], "\n\n", icingaMap["service_output"], "```")
+        fmt.Println(notification)
+}
+
+func host_or_service (icingaMap map[string]string) {
+        if icingaMap["service_name"] != "" {
+                emote := get_emote(icingaMap["service_state"])
+                notification := fmt.Sprintf("%v %v: %v%v%v%v%v%v%v%v%v%v", emote, icingaMap["notification_type"], "Service **", icingaMap["service_name"], "** on host **", icingaMap["host_name"], "** changed to **", icingaMap["service_state"], "**!\n", "```", icingaMap["service_output"], "```")
+        fmt.Println(notification)
+        }
+}
+
+
+
